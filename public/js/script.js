8a79edd6fc8e55028e91e9d54d2e457d56df1873
Jatin Yadav <Jatin.Yadav1307@gmail.com>
2017-09-29 15:40:24 +0530

    Basic functionality of chat application in place

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,42 @@
 // Sends request to the server to open up the web socket connection
 let socket = io();
 
-socket.on('connect', function () {
-    console.log('Connected to the server!')
+// socket.on('connect', function () {
+//     console.log('Connected to the server!')
 
-    // let messageData = {
-    //     from: 'jatin.yadav1307@gmail.com',
-    //     text: 'Hello world from email application',
-    // }
-    // socket.emit('createMessage', messageData)
-})
+// })
 
-socket.on('disconnect', function () {
-    console.log('Disconnected from the server!')
-})
+// socket.on('disconnect', function () {
+//     console.log('Disconnected from the server!')
+// })
 
+// Run this function when a new message arrives from the server to the client
 socket.on('newMessage', function (message) {
-    console.log('New Message!', message)
+    // console.log('New Message!', message)
+    let li = $('<li></li>')
+    li.text(`${message.from}: ${message.text}`)
+
+    $('#messages').append(li);
+})
+
+// let messageData = {
+//     from: 'jatin.yadav1307@gmail.com',
+//     text: 'Hello world from email application',
+// }
+// socket.emit('createMessage', messageData, function () {
+//     console.log('Got it, all good!')
+// })
+
+
+// Get the message from the form and submit it to the server to broadcast to all the user
+$('#message-form').on('submit', function (e) {
+    // Prevents the default event triggering
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function () {
+        console.log('Message Sent!')
+    })
 })
--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
+const {generateMessage} = require('./utils/message')
+
 // Express configurations
 let port = process.env.PORT || 3000;
 let publicPath = path.join(__dirname, '/../public')
@@ -17,27 +19,24 @@ app.use(express.static(publicPath))
 // Web socket methods for emmision or listening of the events
 
 io.on('connection', function (socket) {
-    console.log('New user connected!');
+    // console.log('New user connected!');
 
-    // let messageData = {
-    //     from: 'jatin.yadav1307@gmail.com',
-    //     text: 'Hello world from email application',
-    //     createdAt: 1212312321,
-    // }
-    // socket.emit('newMessage', messageData)
+    // Emitting a new message once the user is connected to welcome to the chat app
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat application!'))
+    // Letting others know that a new user joined
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
-    socket.on('createMessage', function (messsage) {
-        console.log(messsage)
-        io.emit('newMessage', {
-            from: messsage.from,
-            text: messsage.text,
-            createdAt: new Date().getTime()
-        })
+    // Whenever a user creates or send a message, broadcasting it to all the other users as well to view
+    socket.on('createMessage', function (messsage, callback) {
+        // console.log(messsage)
+        io.emit('newMessage', generateMessage(messsage.from, messsage.text))
+        callback();
     })
 
-    socket.on('disconnect', function () {
-        console.log('Disconnected from the client!')
-    })
+    // Run, when a user disconnects from the server
+    // socket.on('disconnect', function () {
+    //     console.log('Disconnected from the client!')
+    // })
 })
 
 // Starting up the server
